5ba7a9d87f3ad6ac4664a230592b84eafef9d77d
Ian Andersen <ianderse@mac.com>
2014-09-24 09:14:19 -0600

    begin email address implementation

---
+++ spec/models/email_address_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe EmailAddress, :type => :model do
+	let(:email_address) do
+	  EmailAddress.new(address: "example@example.com")
+	end
+
+	it 'is valid' do
+  	expect(email_address).to be_valid
+  end
+
+  it 'is invalid without an address' do
+  	email_address.address = nil
+  	expect(email_address).to_not be_valid
+  end
+
+end
